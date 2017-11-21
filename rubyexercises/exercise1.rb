e243c407192d637988f00db512f615cfdab6520e
Alex Kim <askim1019@gmail.com>
2017-11-21 20:29:46 +0000

    Edited triangle_facts.rb, still more work to be done

---
+++ rubyexercises/exercise1.rb
@@ -0,0 +1,11 @@
+# Ruby Core exercise. Finding a string method that is not gsub to exchange string characters for a corresponding set of characters
+
+# Process 1 --> go to ruby docs (ruby core documentation)
+# Scroll down to String methods
+# Browse through the methods to find the right method in explanation
+
+# Sample code:
+
+puts "substitute".sub("te", "tion")
+
+puts "subtitute".tr("t", "s")
--- rubyexercises/exercise2.rb
+++ rubyexercises/exercise2.rb
@@ -1,28 +1,23 @@
-# Killer facts about triangles AWW YEAH
-class Triangle
-	attr_accessor :side1, :side2, :side3
+# Refactor this code 
 
-	def initialize(side1, side2, side3)
-		@side1 = side1
-		@side2 = side2
-		@side3 = side3
+
+class Triangle
+	attr_accessor :side1,:side2,:side3
+
+	def initialize(side1,side2,side3)
+		@side1,@side2,@side3 = side1,side2,side3
 	end
 
 	def equilateral()
-		if side1 == side2 && side2 == side3
-			return true
-		end
+		return side1 == side2 && side2 == side3
 	end
 
-
 	def isosceles()
-		if [side1 ,side2, side3].uniq.length == 2
-			return true
-		end
+		return [side1,side2,side3].uniq.length == 2
 	end
 
 	def scalene()
-		unless !(equilateral || isosceles)
+		unless ! (equilateral || isosceles)
 	    false
 	  else
 	    true
@@ -30,26 +25,18 @@ class Triangle
 	end
 
 	def recite_facts
-		if equilateral
-			puts 'This triangle is equilateral!'
-		end
-		
-		if isosceles
-			puts 'This triangle is isosceles! Also, that word is hard to type.'
-		end
-		
-		if scalene
-			puts 'This triangle is scalene and mathematically boring.' 
-		end
-		
-		angles = self.calculate_angles(side1, side2, side3)
-		
+		puts 'This triangle is equilateral!' if equilateral
+		puts 'This triangle is isosceles! Also, that word is hard to type.' if isosceles 
+		puts 'This triangle is scalene and mathematically boring.' if scalene 
+
+		angles = self.calculate_angles(side1,side2,side3)
 		puts 'The angles of this triangle are ' + angles.join(',')
+
 		puts 'This triangle is also a right triangle!' if angles.include? 90
 		puts ''
 	end
 
-	def calculate_angles(a, b, c)
+	def calculate_angles(a,b,c)
 		angleA = radians_to_degrees(Math.acos((b**2 + c**2 - a**2) / (2.0 * b * c)))
 		angleB = radians_to_degrees(Math.acos((a**2 + c**2 - b**2) / (2.0 * a * c)))
 		angleC = radians_to_degrees(Math.acos((a**2 + b**2 - c**2) / (2.0 * a * b)))
@@ -64,8 +51,8 @@ end
 
 
 triangles = [
-	[5, 5, 5],
-	[5, 12, 13]
+	[5,5,5],
+	[5,12,13],
 ]
 triangles.each { |sides|
 	tri = Triangle.new(*sides)
