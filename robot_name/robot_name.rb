fa0160f4dac5ddcc584a919937df304b521f06ce
Alex Kim <askim1019@gmail.com>
2017-12-14 17:13:20 +0000

    Refactored robot_name.rb to confine to ruby guidelines

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -1,27 +1,42 @@
-class NameCollisionError < RuntimeError; end
+class NameCollisionError < RuntimeError
+end
 
 class Robot
   attr_accessor :name
 
-  @@registry
+  @@registry ||= []
 
   def initialize(args = {})
-    @@registry ||= []
-    @name_generator = args[:name_generator]
-
-    if @name_generator
-      @name = @name_generator.call
-    else
-      generate_char = -> { ('A'..'Z').to_a.sample }
-      generate_num = -> { rand(10) }
-
-      @name = "#{generate_char.call}#{generate_char.call}#{generate_num.call}#{generate_num.call}#{generate_num.call}"
+    @name_generator = args[:name_generator] || -> { normal_name_generator }
+    generate_name
+    register_name
+  end
+  
+  def generate_name
+    @name = @name_generator.call
+    
+    if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
+      raise NameCollisionError, 'There was a problem generating the robot name!'
     end
-
-    raise NameCollisionError, 'There was a problem generating the robot name!' if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
+  end
+  
+  def normal_name_generator
+    "#{generate_char}#{generate_char}#{generate_num}#{generate_num}#{generate_num}"
+  end
+  
+  def generate_char
+    ('A'..'Z').to_a.sample
+  end
+  
+  def generate_num
+    rand(10)
+  end
+  
+  def register_name
     @@registry << @name
   end
 end
+
 
 robot = Robot.new
 puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
